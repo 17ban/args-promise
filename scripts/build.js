9d499d201aab16ef84eb01402d73adda03cac1ba
17BAN <17ban.1024@gmail.com>
2020-11-09 20:34:07 +0800

    rewrite build.js

--- scripts/build.js
+++ scripts/build.js
@@ -1,32 +1,44 @@
-let process = require('child_process')
+const process = require('child_process')
+async function exec(command) {
+    return new Promise(r => {
+        process.exec(command, function(error, stdout, stderr) {
+            if(error) console.log("error:" + error)
+            if(stdout) console.log("stdout:" + stdout)
+            if(stderr) console.log("stderr:" + stderr)
+            r()
+        })
+    })
+}
 
+
+/**
+ * build command
+ */
 let cjs = [
     'tsc -t ES6 -m commonjs -d --lib es2019 --strict --outDir ./dist ./src/ArgsPromise.ts',
-    '&& terser ./dist/ArgsPromise.js -o ./dist/ArgsPromise.min.js -c -m'
-]
-process.exec(cjs.join(' '), function(error, stdout, stderr) {
-    if(error) console.log("error:" + error)
-    if(stdout) console.log("stdout:" + stdout)
-    if(stderr) console.log("stderr:" + stderr)
-})
-
+    '&& cd ./dist',
+    '&& rename ArgsPromise.js ArgsPromise.cjs.js',
+    '&& terser ArgsPromise.cjs.js -o ArgsPromise.cjs.min.js -c -m'
+].join(' ')
 
 let esm = [
-    'tsc -t ES6 -m ES6 --lib es2019 --strict --outDir ./dist ./src/ArgsPromise.esm.ts',
-    '&& terser ./dist/ArgsPromise.esm.js -o ./dist/ArgsPromise.esm.min.js -c -m'
-]
-process.exec(esm.join(' '), function(error, stdout, stderr) {
-    if(error) console.log("error:" + error)
-    if(stdout) console.log("stdout:" + stdout)
-    if(stderr) console.log("stderr:" + stderr)
-})
+    'tsc -t ES6 -m ES6 --lib es2019 --strict --outDir ./dist ./src/ArgsPromise.ts',
+    '&& cd ./dist',
+    '&& rename ArgsPromise.js ArgsPromise.esm.js',
+    '&& terser ArgsPromise.esm.js -o ArgsPromise.esm.min.js -c -m'
+].join(' ')
 
 let browser = [
     'tsc -t ES5 --outDir ./dist --lib es2019 ./src/ArgsPromise.browser.ts',
     '&& terser ./dist/ArgsPromise.browser.js -o ./dist/ArgsPromise.browser.min.js -c -m'
-]
-process.exec(browser.join(' '), function(error, stdout, stderr) {
-    if(error) console.log("error:" + error)
-    if(stdout) console.log("stdout:" + stdout)
-    if(stderr) console.log("stderr:" + stderr)
-})
+].join(' ');
+
+
+/**
+ * build
+ */
+(async () => {
+    exec(browser)
+    await exec(cjs)
+    await exec(esm)
+})()
