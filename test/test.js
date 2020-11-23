f72ee72b6acaaaa4f5bf2b552848f0a8e941f359
17BAN <17ban.1024@gmail.com>
2020-11-24 01:40:48 +0800

    add jest test

--- test/test.js
+++ test/test.js
@@ -1,87 +1,58 @@
 const ArgsPromise = require('../../ArgsPromise').default
 
-// async function test() {
-//     let args = await new ArgsPromise(resolve => {
-//         resolve(1, 2, 3, 4, 5)
-//     }).pack()
-//     console.log(args)  // --> [ 1, 2, 3, 4, 5 ]
-
-//     let p = new ArgsPromise(resolve => {
-//         resolve(1, 2, 3, 4, 5)
-//     }).pack()  
-//     Promise.allSettled([p]).then(result => {
-//         console.log(result[0].value)  // --> [ 1, 2, 3, 4, 5 ]
-//     })
-// }
-// test()
-
-// new ArgsPromise((resolve, reject, resident) => {
-//     setTimeout(() => {
-//         resolve(1, 2, 'hello')
-//     }, 1000)
-//     resident('resident-0', 'resident-1')
-// }).then((...args) => {
-//     console.log(...args)    // 1 2 'hello' 'resident-0' 'resident-1'
-//     return ['foo', 'bar']
-// }).then((...args) => {
-//     console.log(...args)    // 'foo' 'bar' 'resident-0' 'resident-1'
-//     return 123
-// }).catch((...args) => {
-//     console.log(...args)    // 123 'resident-0' 'resident-1'
-// }).then((...args) => {
-//     console.log(...args)    //'resident-0' 'resident-1'
-// })
-
-// new ArgsPromise((resolve, reject) => {
-//     resolve('foo', 'bar')
-// }).then((a, b) => {
-//     console.log(a, b)  // 'foo' 'bar'
-// })
-
-// new ArgsPromise((resolve, reject) => {
-//     reject('foo', 'bar')
-// }).then((a, b) => {
-//     console.log(a, b)  // no output
-// }).catch((a, b) => {
-//     console.log('catched', a, b)  // 'catched' 'foo' 'bar'
-// })
-
-// let p1 = new ArgsPromise(resolve => {
-//     setTimeout(() => {
-//         resolve()
-//     }, 1000)
-// })
-// let p2 = new ArgsPromise(resolve => {
-//     setTimeout(() => {
-//         resolve()
-//     }, 3000)
-// })
-// Promise.allSettled([p1, p2])
-//     .then(() => {
-//         console.log('allSettled')
-//     })
-
-// async function foo() {
-//     await new ArgsPromise((resolve) => {
-//         setTimeout(() => {
-//             resolve()
-//         }, 3000)
-//     })
-//     console.log('bar')
-// }
-// foo()
-
-
-
-async function t() {
-    let p1 = new ArgsPromise((resolve, reject) => {
-        resolve('hello', 'world')
+test('resolve and resident', () => {
+    new ArgsPromise((r, rj, rd) => {
+        rd('rd1', 'rd2')
+        r('foo', 'bar')
+    }).then((...args) => {
+        expect(args).toEqual(['foo', 'bar', 'rd1', 'rd2'])
+    }).then((...args) => {
+        expect(args).toEqual(['rd1', 'rd2'])
     })
-    console.log(await p1.to())  // --> [ null, [ 'hello', 'world' ] ]
+})
 
-    let p2 = new ArgsPromise((resolve, reject) => {
-        reject('err')
+test('reject', () => {
+    let a = 0
+    new ArgsPromise((r, rj) => {
+        rj('foo', 'bar')
+    }).then(() => {
+        a++
+    }).then(() => {
+        a++
+    }).catch((...args) => {
+        expect(args).toEqual(['foo', 'bar'])
+        expect(a).toBe(0)
     })
-    console.log(await p2.to())  // --> [ [ 'err' ], undefined ]
-}
-t()
+})
+
+test('then', () => {
+    new ArgsPromise(r => {
+        r()
+    }).then(() => {
+        return 123
+    }).then(arg => {
+        expect(arg).toBe(123)
+        return ['foo', 'bar', null, 22]
+    }).then((...args) => {
+        expect(args).toEqual(['foo', 'bar', null, 22])
+    })
+})
+
+test('pack', async () => {
+    let args = await new ArgsPromise(r => {
+        r('a', 'b', 123)
+    }).pack()
+    expect(args).toEqual(['a', 'b', 123])
+})
+
+test('to', async () => {
+    let args = await new ArgsPromise(r => {
+        r('a', 'b', 123)
+    }).to()
+    expect(args).toEqual([null, ['a', 'b', 123]])
+
+    args = await new ArgsPromise((r, rj) => {
+        rj('reason', 123)
+    }).to()
+    expect(args).toEqual([['reason', 123], undefined])
+})
