ebe7388b3f06674d7bc9085560268e71174c93f3
17BAN <17ban.1024@gmail.com>
2020-12-12 22:38:12 +0800

    optimize build.js

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,9 @@
 const process = require('child_process')
 async function exec(command) {
     return new Promise(r => {
-        process.exec(command, function(error, stdout, stderr) {
+        process.exec(command, { encoding: 'utf-8' }, (error, stdout, stderr) => {
             if(error) console.log("error:" + error)
-            if(stdout) console.log("stdout:" + stdout)
+            // if(stdout) console.log("stdout:" + stdout)
             if(stderr) console.log("stderr:" + stderr)
             r()
         })
@@ -14,22 +14,29 @@ async function exec(command) {
 /**
  * build command
  */
+let delDist = [
+    'del /q dist\\*'
+].join(' ')
+
 let cjs = [
-    'tsc -t ES6 -m commonjs -d --lib es2019 --strict --outDir ./dist ./src/ArgsPromise.ts',
+    'chcp 65001',
+    '&& tsc -t ES6 -m commonjs -d --lib es2019 --strict --outDir ./dist ./src/ArgsPromise.ts',
     '&& cd ./dist',
-    '&& rename ArgsPromise.js ArgsPromise.cjs.js',
+    '&& ren ArgsPromise.js ArgsPromise.cjs.js',
     '&& terser ArgsPromise.cjs.js -o ArgsPromise.cjs.min.js -c -m'
 ].join(' ')
 
 let esm = [
-    'tsc -t ES6 -m ES6 --lib es2019 --strict --outDir ./dist ./src/ArgsPromise.ts',
+    'chcp 65001',
+    '&& tsc -t ES6 -m ES6 --lib es2019 --strict --outDir ./dist ./src/ArgsPromise.ts',
     '&& cd ./dist',
-    '&& rename ArgsPromise.js ArgsPromise.esm.js',
+    '&& ren ArgsPromise.js ArgsPromise.esm.js',
     '&& terser ArgsPromise.esm.js -o ArgsPromise.esm.min.js -c -m'
 ].join(' ')
 
 let browser = [
-    'tsc -t ES5 --outDir ./dist --lib es2019 ./src/ArgsPromise.browser.ts',
+    'chcp 65001',
+    '&& tsc -t ES5 --outDir ./dist --lib es2019 ./src/ArgsPromise.browser.ts',
     '&& terser ./dist/ArgsPromise.browser.js -o ./dist/ArgsPromise.browser.min.js -c -m'
 ].join(' ');
 
@@ -38,6 +45,7 @@ let browser = [
  * build
  */
 (async () => {
+    await exec(delDist)
     exec(browser)
     await exec(cjs)
     await exec(esm)
